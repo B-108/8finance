f350e83f492fc9435401538dff6f85261deb19b7
ldg <eorud2546@nate.com>
2023-09-12 16:31:28 +0900

    [feat] 계좌 관련 기능 구현 230912 1631

---
+++ Billage/Back/billage/src/main/java/com/fin/billage/domain/account/api/AccountController.java
@@ -0,0 +1,52 @@
+package com.fin.billage.domain.account.api;
+
+import com.fin.billage.domain.account.dto.AccountRequestDto;
+import com.fin.billage.domain.account.dto.AccountResponseDto;
+import com.fin.billage.domain.account.entity.Account;
+import com.fin.billage.domain.account.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/account")
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService accountService;
+
+    // 내 계좌 등록
+    @PostMapping
+    public ResponseEntity<Account> addMyAccount(@RequestBody AccountRequestDto dto) {
+        Account account = accountService.addMyAccount(dto);
+        return new ResponseEntity<>(account, HttpStatus.OK);
+    }
+
+    // 내 계좌 조회
+    @GetMapping
+    public ResponseEntity<List<AccountResponseDto>> searchMyAccount() {
+        List<AccountResponseDto> accounts = accountService.searchMyAccount();
+        return new ResponseEntity<>(accounts, HttpStatus.OK);
+    }
+
+    // 주 계좌 등록
+    @PatchMapping("/{account_id}")
+    public ResponseEntity<Account> addMyMainAccount(
+            @PathVariable Long account_id
+    ) {
+        Account account = accountService.addMyMainAccount(account_id);
+        return new ResponseEntity<>(account, HttpStatus.OK);
+    }
+
+    // 계좌 삭제
+    @PatchMapping("delete/{account_id}")
+    public ResponseEntity<Account> deleteMyAccount(
+            @PathVariable Long account_id
+    ) {
+        Account account = accountService.deleteMyAccount(account_id);
+        return new ResponseEntity<>(account, HttpStatus.OK);
+    }
+}
--- Billage/Back/billage/src/main/java/com/fin/billage/domain/account/service/AccountService.java
+++ Billage/Back/billage/src/main/java/com/fin/billage/domain/account/service/AccountService.java
@@ -1,4 +1,107 @@
 package com.fin.billage.domain.account.service;
 
+import com.fin.billage.domain.account.dto.AccountRequestDto;
+import com.fin.billage.domain.account.dto.AccountResponseDto;
+import com.fin.billage.domain.account.entity.Account;
+import com.fin.billage.domain.account.repository.AccountRepository;
+import com.fin.billage.domain.user.entity.User;
+import com.fin.billage.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class AccountService {
+
+    private final AccountRepository accountRepository;
+    private final UserRepository userRepository;
+
+    // 내 계좌 등록
+    public Account addMyAccount(AccountRequestDto dto) {
+        Long user_pk = 1111L;
+        User user = userRepository.findById(user_pk);
+
+        boolean mainYn = false;
+//         가지고 있는 계좌가 null(0개) 일 때 첫 번째 등록 계좌를 주계좌로
+        Optional<Account> userAccounts = accountRepository.findByUser(user);
+        if (!userAccounts.isPresent()) {
+            mainYn = true;
+        }
+
+        Account account = Account.builder()
+                .user(user)
+                .accountBankCode(dto.getAccountBankCode())
+                .accountNum(dto.getAccountNum())
+                .accountRegistDate(LocalDateTime.now())
+                .accountMainYn(mainYn)
+                .build();
+
+
+        accountRepository.save(account);
+        return account;
+    }
+
+    // 내 계좌 리스트 조회
+    public List<AccountResponseDto> searchMyAccount() {
+        Long user_pk = 1111L;
+        User user = userRepository.findById(user_pk);
+        List<Account> accounts = accountRepository.findAllByUserId(user);
+        List<AccountResponseDto> dtos = new ArrayList<>();
+
+        for (Account account : accounts) {
+            AccountResponseDto dto = AccountResponseDto.builder()
+                    .accountBankCode(account.getAccountBankCode())
+                    .accountNum(account.getAccountNum())
+                    .accountRegistDate(account.getAccountRegistDate())
+                    .accountMainYn(account.getAccountMainYn())
+                    .build();
+
+            dtos.add(dto);
+        }
+
+        return dtos;
+    }
+
+    // 주 계좌 등록
+    public Account addMyMainAccount(Long account_id) {
+        Long user_pk = 1111L;
+        User user = userRepository.findById(user_pk).orElse(null);
+
+        // 기존 주계좌 찾아서 false로 바꿔주기
+        Account mainAccount = accountRepository.findByUserAndAccountMainYnIsTrue(user);
+
+        if (mainAccount != null) {
+            mainAccount.updateAccountMainYn(false);
+            accountRepository.save(mainAccount);
+        }
+
+        // 주계좌로 설정
+        Account account = accountRepository.findByUserAndAccountId(user, account_id);
+        if (account != null) {
+            account.updateAccountMainYn(true);
+            accountRepository.save(account);
+        }
+
+        return account;
+    }
+
+    // 등록된 계좌 삭제처리
+    public Account deleteMyAccount(Long account_id) {
+        Long user_pk = 1111L;
+        User user = userRepository.findById(user_pk).orElse(null);
+        Account account = accountRepository.findByUserAndAccountId(user, account_id);
+
+        account.deleteAccount();
+        accountRepository.save(account);
+
+        return account;
+    }
+
+    //
+
 }
