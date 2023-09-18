738ea65c3d2b48bc688249a84959b5de5549af05
정현우 <gusehd502@naver.com>
2023-09-18 14:59:04 +0900

    feat : Router페이지 준비

--- Billage/Front/billage/src/main.tsx
+++ Billage/Front/billage/src/main.tsx
@@ -4,16 +4,14 @@ import ReactDOM from 'react-dom/client'
 import { RecoilRoot } from "recoil";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-
 import theme from './themes/index.ts';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <RecoilRoot>
-       <RouterProvider router={router}/>
+        <RouterProvider router={router}/>
       </RecoilRoot>
     </ThemeProvider>
   </React.StrictMode>,
