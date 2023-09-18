8e0dafe964bd74a712493ece27d1d98b9ae95339
wndjs8225 <wndjs8225@naver.com>
2023-09-18 15:24:14 +0900

    feat   : 절대경로 재설정

--- Billage/Front/billage/src/Router.tsx
+++ Billage/Front/billage/src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
-import Root from "./App";
-import MainPage from "./pages/Main/Main";
+import Root from "@src/App";
+import MainPage from "@pages/Main/Main";
+
 // import ErrorComponent from "./components/ErrorComponent";
 // import NotFound from "./pages/NotFound"
 // import Home from "./pages/Home";
--- Billage/Front/billage/vite.config.ts
+++ Billage/Front/billage/vite.config.ts
@@ -6,13 +6,15 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [
-      { find: '@src', replacement: resolve(__dirname, 'src') },
-      {
-        find: '@components',
-        replacement: resolve(__dirname, 'src/components'),
-      },
-    ],
+    alias: {
+      '@': resolve(__dirname, ''),
+      '@src': resolve(__dirname, 'src/'),
+      '@pages': resolve(__dirname, 'src/pages/'),
+      '@components': resolve(__dirname, 'src/components/'),
+      '@assets': resolve(__dirname, 'src/assets/'),
+      '@themes': resolve(__dirname, 'src/themes/'),
+      '@public': resolve(__dirname, 'public/'),
+    }
   },
 
   plugins: [react(), tsconfigPaths()],
