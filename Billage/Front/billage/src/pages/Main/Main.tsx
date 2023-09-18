698f47b3a0f31b73a8637c197caab50145ad99ed
wndjs8225 <wndjs8225@naver.com>
2023-09-18 14:44:50 +0900

    feat   : 절대 경로 설정

---
+++ Billage/Front/billage/src/pages/Main/Main.tsx
@@ -0,0 +1,11 @@
+// import Input from "/src/components/Common/Input"
+function MainPage(){
+    return(
+        <>
+        <h1>Main Page</h1>
+        {/* <Input></Input> */}
+        </>
+    )
+}
+
+export default MainPage
