00cd64e510e69dec8dd41765cf0d0a815b9f033b
정현우 <gusehd502@naver.com>
2023-09-19 13:24:56 +0900

    feat : themes의 폰트랑 모서리 사이즈 지정

--- Billage/Front/billage/src/themes/fontSize.ts
+++ Billage/Front/billage/src/themes/fontSize.ts
@@ -1,12 +1,14 @@
 const fontSize = {
-  extraLarge: '28px',
-  large: '26px',
-  medium: '18px',
-  default: '16px',
-  small: '14px',
-  extraSmall: '13px',
+  XL: '28px',
+  L: '24px',
+  M: '20px',
+  DF: '16px',
+  S: '14px',
+  XS: '12px',
+  XXS: '10px',
 } as const;
 
 export type fontSizeThemeKey = keyof typeof fontSize;
 
 export default fontSize;
+
--- Billage/Front/billage/src/themes/index.ts
+++ Billage/Front/billage/src/themes/index.ts
@@ -1,8 +1,11 @@
 import color from "src/themes/color";
+import fontSize from "src/themes/fontSize";
+import radius from "src/themes/radius";
 
 const theme = {
   color,
-  
+  fontSize,
+  radius
 } as const;
 
 export type AppTheme = typeof theme;
