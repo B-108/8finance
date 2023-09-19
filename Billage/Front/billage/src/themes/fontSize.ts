6d6200074c8a7888a90cb4f60874312448c6d850
정현우 <gusehd502@naver.com>
2023-09-19 15:14:59 +0900

    feat : 폰트사이즈rem 변경

--- Billage/Front/billage/src/themes/fontSize.ts
+++ Billage/Front/billage/src/themes/fontSize.ts
@@ -1,11 +1,11 @@
 const fontSize = {
-  XL: '28px',
-  L: '24px',
-  M: '20px',
-  DF: '16px',
-  S: '14px',
-  XS: '12px',
-  XXS: '10px',
+  XL: "1.75rem",    /* 28px / 16px = 1.75rem */
+  L: "1.5rem",      /* 24px / 16px = 1.5rem */
+  M: "1.25rem",     /* 20px / 16px = 1.25rem */
+  DF: "1rem",       /* 16px / 16px = 1rem */
+  S: "0.875rem",    /* 14px / 16px = 0.875rem */
+  XS: "0.75rem",    /* 12px / 16px = 0.75rem */
+  XXS: "0.625rem",  /* 10px / 16px = 0.625rem */
 } as const;
 
 export type fontSizeThemeKey = keyof typeof fontSize;
