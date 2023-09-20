3fb6817f61f539f6a303b1a3b1f09b74d5260a3b
이경원 <jbs6606@naver.com>
2023-09-20 16:59:50 +0900

    fix : 충돌해결

--- Billage/Front/billage/src/pages/IOU/IOU.tsx
+++ Billage/Front/billage/src/pages/IOU/IOU.tsx
@@ -1,44 +1,42 @@
-import { InputDiv, InputTitle } from "./IOU.style";
-import Input from "/src/components/Common/Input";
-import { useState } from "react";
+import { InputDiv, InputTitle } from './IOU.style';
+import Input from '/src/components/Common/Input';
+import { useState } from 'react';
 
-function IOU(){
-  const [friendInfo, setFriendInfo] = useState<string>('')
-  const [accountInfo, setAccountInfo] = useState<string>('');
+function IOU() {
+    const [friendInfo, setFriendInfo] = useState<string>('');
+    const [accountInfo, setAccountInfo] = useState<string>('');
 
-  const handleFriendInfoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setFriendInfo(event.target.value);
-  };
-  const handleAccountInfoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setAccountInfo(event.target.value);
-  };
+    const handleFriendInfoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFriendInfo(event.target.value);
+    };
+    const handleAccountInfoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAccountInfo(event.target.value);
+    };
 
-  return (
-      <>
-          <InputDiv>
-              <InputTitle>지인선택</InputTitle>
-              <Input value={friendInfo} $active $size="90%,40px" onChange={handleFriendInfoChange}></Input>
-          </InputDiv>
-          <InputDiv>
-              <InputTitle>돈 받을 계좌</InputTitle>
-              <Input value={accountInfo} $active $size="90%,40px" onChange={handleAccountInfoChange}></Input>
-          </InputDiv>
-          <InputDiv>
-              <InputTitle>돈 갚을 날짜</InputTitle>
-              <Input value={friendInfo} $active $size="90%,40px" onChange={handleFriendInfoChange}></Input>
-          </InputDiv>
-          <InputDiv>
-              <InputTitle>자동이체</InputTitle>
-              <Input value={friendInfo} $active $size="90%,40px" onChange={handleFriendInfoChange}></Input>
-          </InputDiv>
-          <InputDiv>
-              <InputTitle>빌릴 금액</InputTitle>
-              <Input value={friendInfo} $active $size="90%,40px" onChange={handleFriendInfoChange}></Input>
-          </InputDiv>
-      </>
-  );
+    return (
+        <>
+            <InputDiv>
+                <InputTitle>지인선택</InputTitle>
+                <Input value={friendInfo} $active $size="90%,40px" onChange={handleFriendInfoChange}></Input>
+            </InputDiv>
+            <InputDiv>
+                <InputTitle>돈 받을 계좌</InputTitle>
+                <Input value={accountInfo} $active $size="90%,40px" onChange={handleAccountInfoChange}></Input>
+            </InputDiv>
+            <InputDiv>
+                <InputTitle>돈 갚을 날짜</InputTitle>
+                <Input value={friendInfo} $active $size="90%,40px" onChange={handleFriendInfoChange}></Input>
+            </InputDiv>
+            <InputDiv>
+                <InputTitle>자동이체</InputTitle>
+                <Input value={friendInfo} $active $size="90%,40px" onChange={handleFriendInfoChange}></Input>
+            </InputDiv>
+            <InputDiv>
+                <InputTitle>빌릴 금액</InputTitle>
+                <Input value={friendInfo} $active $size="90%,40px" onChange={handleFriendInfoChange}></Input>
+            </InputDiv>
+        </>
+    );
 }
 
-export default IOU
-
-
+export default IOU;
