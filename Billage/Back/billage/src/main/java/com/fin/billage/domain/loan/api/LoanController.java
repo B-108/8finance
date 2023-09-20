ddf81621be5f104573649552100ec0a9d734473a
ldg <eorud2546@nate.com>
2023-09-20 17:47:27 +0900

    Feat : 거래 목록 기능 추가

--- Billage/Back/billage/src/main/java/com/fin/billage/domain/loan/api/LoanController.java
+++ Billage/Back/billage/src/main/java/com/fin/billage/domain/loan/api/LoanController.java
@@ -1,4 +1,33 @@
-package com.fin.billage.domain.loan.api;
-
-public class LoanController {
-}
+//package com.fin.billage.domain.loan.api;
+//
+//import com.fin.billage.domain.loan.dto.LoanResponseDto;
+//import com.fin.billage.domain.loan.repository.LoanRepository;
+//import com.fin.billage.domain.loan.service.LoanService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/loan")
+//public class LoanController {
+//
+//    private final LoanService loanService;
+//    // 내가 채권자면 빌려준 목록이고
+//    // 내가 채무자면 빌려준 목록
+//
+//    // 빌려준 거래 조회
+//    @GetMapping("/lend")
+//    public ResponseEntity<List<LoanResponseDto>> searchLendList(HttpServletRequest request) {
+//        List<LoanResponseDto> loanResponseDtoList = loanService.searchLendList(request);
+//        return new ResponseEntity<>(loanResponseDtoList, HttpStatus.OK);
+//    }
+//    // 빌린 거래 조회
+//}
--- Billage/Back/billage/src/main/java/com/fin/billage/domain/loan/service/LoanService.java
+++ Billage/Back/billage/src/main/java/com/fin/billage/domain/loan/service/LoanService.java
@@ -1,4 +1,26 @@
-package com.fin.billage.domain.loan.service;
-
-public class LoanService {
-}
+//package com.fin.billage.domain.loan.service;
+//
+//import com.fin.billage.domain.loan.dto.LoanResponseDto;
+//import com.fin.billage.domain.loan.repository.LoanRepository;
+//import com.fin.billage.domain.user.entity.User;
+//import com.fin.billage.domain.user.repository.UserRepository;
+//import com.fin.billage.util.JwtUtil;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import java.util.List;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class LoanService {
+//    private final LoanRepository loanRepository;
+//    private final UserRepository userRepository;
+//    private final JwtUtil jwtUtil;
+//
+//    public List<LoanResponseDto> searchLendList(HttpServletRequest request) {
+//        Long user_pk = jwtUtil.extractUserPkFromToken(request);
+//        User user = userRepository.findById(user_pk).orElse(null);
+//
+//    }
+//}
