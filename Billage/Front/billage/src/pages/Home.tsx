6211277dfb8f2f602dc3f9e7ddcd3b9011f5779a
정현우 <gusehd502@naver.com>
2023-09-20 16:46:33 +0900

    feat : 리코일 셋팅

--- Billage/Front/billage/src/pages/Home.tsx
+++ Billage/Front/billage/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Button from '../components/Common/Button';
 import Image from '../components/Common/Image';
 
 function Home() {
@@ -5,15 +6,24 @@ function Home() {
         <>
             <div>
                 여기가 HOME
-                <Image type="KB" src="src/assets/KB.svg" alt="KB" width="200px" height="200px" $rounded></Image>
+                <Image 
+                    type="KB" 
+                    src="src/assets/KB.svg" 
+                    alt="KB" 
+                    width="300px" 
+                    height="200px" 
+                    $rounded></Image>
                 <Image
                     type="wallet"
                     src="src/assets/wallet.svg"
                     alt="wallet"
                     width="200px"
-                    height="200px"
+                    height="500px"
                     $rounded
                 ></Image>
+                <Button
+                    $Green
+                    $size="200px,50px">안녕하세요</Button>
             </div>
         </>
     );
