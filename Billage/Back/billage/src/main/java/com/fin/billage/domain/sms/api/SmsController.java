19fa6a0ebde3f949fe72da1489b320564c12f499
ldg <eorud2546@nate.com>
2023-09-21 12:39:49 +0900

    Feat : 문자 인증 기능 추가

---
+++ Billage/Back/billage/src/main/java/com/fin/billage/domain/sms/api/SmsController.java
@@ -0,0 +1,47 @@
+package com.fin.billage.domain.sms.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fin.billage.domain.sms.dto.SmsDto;
+import com.fin.billage.domain.sms.dto.SmsResponseDto;
+import com.fin.billage.domain.sms.dto.SmsVerifyDto;
+import com.fin.billage.domain.sms.service.SmsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/sms")
+public class SmsController {
+    private final SmsService smsService;
+
+    // 문자인증 보내기
+    @PostMapping
+    public SmsResponseDto sendSms(@RequestBody SmsDto messageDto) throws UnsupportedEncodingException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException, JsonProcessingException {
+
+        SmsResponseDto responseDto = smsService.sendSms(messageDto);
+        return responseDto;
+    }
+
+    // 문자인증
+    @PostMapping("/verify")
+    public ResponseEntity<Void> verifySms(@RequestBody SmsVerifyDto verifyDto) throws UnsupportedEncodingException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException, JsonProcessingException {
+
+        boolean isSmsVerified = smsService.verifySms(verifyDto);
+
+        if (isSmsVerified) {
+            // 성공한 경우 200 OK 반환
+            return ResponseEntity.ok().build();
+        } else {
+            // 실패한 경우 400 Bad Request 반환
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+
+}
