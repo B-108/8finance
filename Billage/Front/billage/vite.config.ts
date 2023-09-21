ac5ba49c4693fa010d18c47eae662d7f2bf87396
송혁준 <thdgurws98@naver.com>
2023-09-21 22:04:09 +0900

    Build : vite config file 수정 230921 2204

--- Billage/Front/billage/vite.config.ts
+++ Billage/Front/billage/vite.config.ts
@@ -16,4 +16,12 @@ export default defineConfig({
   },
 
   plugins: [react(), tsconfigPaths()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  }
 });
