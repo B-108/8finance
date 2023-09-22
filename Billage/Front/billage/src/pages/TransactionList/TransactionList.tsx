cca0fb4c4eb33be0fb26fff258363b85baf6379d
wndjs8225 <wndjs8225@naver.com>
2023-09-22 12:45:03 +0900

    feat   : 거래목록 뼈대 생성

--- Billage/Front/billage/src/pages/TransactionList/TransactionList.tsx
+++ Billage/Front/billage/src/pages/TransactionList/TransactionList.tsx
@@ -1,31 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import ProgressBar from './ProgressBar';
+import Header from "/src/components/Header/Header"
+import Text from "/src/components/Common/Text"
+import Button from "/src/components/Common/Button"
+import {useState} from 'react'
 
 function TransactionList() {
-    const [progress, setProgress] = useState(0);
+    const [toggle, setToggle] = useState(true)
 
-    useEffect(() => {
-        // 여기에서 진행 상태를 업데이트하는 로직을 추가합니다.
-        // 예를 들어, setTimeout을 사용하여 시간이 지남에 따라 진행 상태를 업데이트할 수 있습니다.
-        const interval = setInterval(() => {
-            if (progress < 100) {
-                setProgress(progress + 10);
-            } else {
-                clearInterval(interval);
-            }
-        }, 1000);
+    const handleBorrowedClick = () => {
+        setToggle(true);
+      };
+    
+      const handleLentClick = () => {
+        setToggle(false);
+      };
 
-        return () => {
-            clearInterval(interval);
-        };
-    }, [progress]);
+    return(
+        <>
+            <Header headerTitle="거래목록"/>
 
-    return (
-        <div>
-            <h1>Progress Bar Example</h1>
-            <ProgressBar progress={progress} />
-        </div>
-    );
+            <div style={{display: 'flex', justifyContent:"space-evenly"}}>
+                <Text $PinText>어떤 거래야?</Text>
+
+                <div style={{display:'flex', alignItems:'center', justifyItems:'center'}}>
+                        <Button
+                            $smallGrayBtn
+                            onClick={handleBorrowedClick}
+                            $Gray={toggle} 
+                        >
+                        빌린목록</Button>
+                        <Button
+                            $smallGrayBtn
+                            onClick={handleLentClick}
+                            $Green={!toggle} 
+                        >
+                        빌려준목록</Button>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default TransactionList;
+export default TransactionList
+
+
+// import React, { useState, useEffect } from 'react';
+// import ProgressBar from './ProgressBar';
+
+// function TransactionList() {
+//     const [progress, setProgress] = useState(0);
+
+//     useEffect(() => {
+//         // 여기에서 진행 상태를 업데이트하는 로직을 추가합니다.
+//         // 예를 들어, setTimeout을 사용하여 시간이 지남에 따라 진행 상태를 업데이트할 수 있습니다.
+//         const interval = setInterval(() => {
+//             if (progress < 100) {
+//                 setProgress(progress + 10);
+//             } else {
+//                 clearInterval(interval);
+//             }
+//         }, 1000);
+
+//         return () => {
+//             clearInterval(interval);
+//         };
+//     }, [progress]);
+
+//     return (
+//         <div>
+//             <h1>Progress Bar Example</h1>
+//             <ProgressBar progress={progress} />
+//         </div>
+//     );
+// }
+
+// export default TransactionList;
