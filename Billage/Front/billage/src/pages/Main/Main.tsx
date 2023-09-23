ed19cee4e5b7ab48b7f14cf35a6afa1868a15d71
정현우 <gusehd502@naver.com>
2023-09-23 23:26:30 +0900

    feat : router 버튼마다 추가

--- Billage/Front/billage/src/pages/Main/Main.tsx
+++ Billage/Front/billage/src/pages/Main/Main.tsx
@@ -10,117 +10,118 @@ import Footer from "/src/components/Common/Footer"
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
 import { AlarmContent, AlarmDate, AlarmHeader, Content, ContentBox, TransactionBox } from "./Main.style"
+import { useNavigate } from "react-router-dom"
 
 function Main(){
-    
-    return(
-        <>
-            <CenteredContainer>
-                <Header
-                    headerTitle="Billage"
-                    ></Header>
-                
-                <Text
-                    $title
-                    $size="98%,50px">확인 하지 않은 알림!</Text>
+  const navigate = useNavigate()
+  const moveTransfer = () => {navigate(`/transfer`)}
+  const moveTransactionList = () => {navigate(`/transactionlist`)}
+  const moveNotifications = () => {navigate(`/notifications`)}
 
-                <Box
-                    $alarmBox
-                    $size="89%,80px">
-                    <AlarmHeader>
-                        <Image
-                            src={alarmBell2}
-                            alt="alarmClock"
-                            width="7%"
-                        ></Image>
-                        <AlarmDate>2023.09.08</AlarmDate>
-                    </AlarmHeader>
-                    <AlarmContent>최싸피님에게 300,000원을 빌려달는 요청이 왔어요!</AlarmContent>
-                </Box>
+  return(
+    <>
+      <CenteredContainer>
+        <Header
+          headerTitle="Billage"
+          ></Header>
+        
+        <Text
+          $title
+          $size="98%,50px">확인 하지 않은 알림!</Text>
 
-                <Text
-                    $title
-                    $size="98%,50px">나의거래</Text>
+        <Box
+          onClick={moveNotifications}
+          $alarmBox
+          $size="89%,80px">
+          <AlarmHeader>
+            <Image
+              src={alarmBell2}
+              alt="alarmClock"
+              width="7%"
+            ></Image>
+            <AlarmDate>2023.09.08</AlarmDate>
+          </AlarmHeader>
+          <AlarmContent>최싸피님에게 300,000원을 빌려달는 요청이 왔어요!</AlarmContent>
+        </Box>
 
-                {/* <Box
-                    $mainTransaction
-                    $size="285px,270px">
+        <Text
+          $title
+          $size="98%,50px">나의거래</Text>
 
-                </Box> */}
+        <div style={{width:"106%"}}>
+          <Splide
+            options={ {
+              focus  : 'center',
+              rewind: true,
+              arrows : false,
+              gap   : '5%',
+              padding: '12%',
+            } }
+            aria-label="My Favorite Images">
+            <SplideSlide>
+              <Box
+                $mainTransaction
+                $size="100%,270px">
+              </Box>
+            </SplideSlide>
+            <SplideSlide>
+              <Box
+                $mainTransaction
+                $size="100%,270px">
+              </Box>
+            </SplideSlide>
+            <SplideSlide>
+              <Box
+                $mainTransaction
+                $size="100%,270px">
+              </Box>
+            </SplideSlide>
+          </Splide>
+        </div>
 
-                <div style={{width:"100%"}}>
-                    <Splide
-                        options={ {
-                            rewind: true,
-                            arrows : false,
-                            gap   : '4%',
-                            padding: '9%',
-                        } }
-                        aria-label="My Favorite Images">
-                        <SplideSlide>
-                            <Box
-                                $mainTransaction
-                                $size="280px,270px">
-                            </Box>
-                        </SplideSlide>
-                        <SplideSlide>
-                            <Box
-                                $mainTransaction
-                                $size="280px,270px">
-                            </Box>
-                        </SplideSlide>
-                        <SplideSlide>
-                            <Box
-                                $mainTransaction
-                                $size="280px,270px">
-                            </Box>
-                        </SplideSlide>
-                        <SplideSlide>
-                            <Box
-                                $mainTransaction
-                                $size="280px,270px">
-                            </Box>
-                        </SplideSlide>
-                    </Splide>
-                </div>
-                <TransactionBox>
-                    <Box
-                        $transaction
-                        $size="46%,60px">
-                        <ContentBox>
-                            <Text
-                                $smallTitle
-                                $size="90%,30px">거래하기</Text>
-                            <Content>
-                                가까운 지인에게 부담없이 빌리세요
-                            </Content>
-                        </ContentBox>
-                        <Image
-                            src={wallet}
-                            alt="wallet"
-                            width="40%"></Image>
-                    </Box>
-                    <Box
-                        $transaction
-                        $size="46%,60px">
-                        <ContentBox>
-                            <Text
-                                $smallTitle
-                                $size="90%,30px">거래목록</Text>
-                            <Content>
-                                지인간의 돈거래 까먹지 마세요!
-                            </Content>
-                        </ContentBox>    
-                        <Image
-                            src={Document}
-                            alt="Document"
-                            width="33%"></Image>
-                    </Box>
-                </TransactionBox>
-            </CenteredContainer>
-            <Footer/>
-        </>
-    )
+        <TransactionBox>
+
+          <Box
+            onClick={moveTransfer}
+            $transaction
+            $size="46%,60px">
+            <ContentBox>
+              <Text
+                $smallTitle
+                $size="90%,30px">거래하기</Text>
+              <Content>
+                가까운 지인에게 부담없이 빌리세요
+              </Content>
+            </ContentBox>
+            <Image
+              src={wallet}
+              alt="wallet"
+              width="39%"></Image>
+          </Box>
+
+          <Box
+            onClick={moveTransactionList}
+            $transaction
+            $size="46%,60px">
+            <ContentBox>
+              <Text
+                $smallTitle
+                $size="90%,30px">거래목록</Text>
+              <Content>
+                지인간의 돈거래 까먹지 마세요!
+              </Content>
+            </ContentBox>    
+            <Image
+              src={Document}
+              alt="Document"
+              width="33%"></Image>
+          </Box>
+
+        </TransactionBox>
+      </CenteredContainer>
+      <Footer/>
+    </>
+  )
 }
 
 export default Main
