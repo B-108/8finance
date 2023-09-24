5f78b348010484013b1bc43b27048cd0fbe86757
이경원 <jbs6606@naver.com>
2023-09-25 03:07:00 +0900

    feat : 계좌등록 기능추가중

--- Billage/Front/billage/src/pages/MyAccounts/MyAccounts.tsx
+++ Billage/Front/billage/src/pages/MyAccounts/MyAccounts.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { Accounts, AccountsContainer, RegistButton, TextDiv } from './MyAccounts.style';
 import CenteredContainer from '/src/components/Common/CenterAlign';
 import Footer from '/src/components/Common/Footer';
@@ -6,6 +7,15 @@ import sampleAccount from '/src/assets/sampleAccount.svg';
 import sampleAccount2 from '/src/assets/sampleAccount2.svg';
 
 function MyAccounts() {
+    const [isAccountClicked, setIsAccountClicked] = useState(false);
+
+    const handleAccountClick = () => {
+        setIsAccountClicked(true); // 클릭 시 테두리 색 변경
+        setTimeout(() => {
+            setIsAccountClicked(false); // 3초 후에 테두리 색 원래대로 복구
+        }, 3000); // 3초 동안 유지
+    };
+
     return (
         <>
             <CenteredContainer>
@@ -13,9 +23,16 @@ function MyAccounts() {
                 <TextDiv>카드를 꾸욱 눌러서 주 계좌를 설정해 보세요!</TextDiv>
                 <RegistButton>계좌 등록</RegistButton>
                 <AccountsContainer>
-                    <Accounts src={sampleAccount}></Accounts>
-                    <Accounts src={sampleAccount2}></Accounts>
-                    {/* <Accounts src={sampleAccount2}></Accounts> */}
+                    <Accounts
+                        src={sampleAccount}
+                        onClick={handleAccountClick}
+                        $isClicked={isAccountClicked} // 계좌 클릭 여부 상태 전달
+                    ></Accounts>
+                    <Accounts
+                        src={sampleAccount2}
+                        onClick={handleAccountClick}
+                        $isClicked={isAccountClicked} // 계좌 클릭 여부 상태 전달
+                    ></Accounts>
                 </AccountsContainer>
                 <Footer></Footer>
             </CenteredContainer>
