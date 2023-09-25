e30c71530cd30766d0a068ad34d416eb266af93a
ldg <eorud2546@nate.com>
2023-09-26 01:13:45 +0900

    Feat : 계좌 불러오기 구현

---
+++ MyData/Back/mydata/src/main/java/com/fin/mydata/domain/log/api/LogController.java
@@ -0,0 +1,26 @@
+package com.fin.mydata.domain.log.api;
+
+import com.fin.mydata.domain.log.dto.AccountRequestDto;
+import com.fin.mydata.domain.log.dto.AccountResponseDto;
+import com.fin.mydata.domain.log.service.LogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/log")
+public class LogController {
+
+    private final LogService logService;
+
+    // 계좌 불러오기 (은행으로 요청)
+    @PostMapping("/getBank")
+    public ResponseEntity<List<AccountResponseDto>> searchBank(@RequestBody List<AccountRequestDto> list) {
+        List<AccountResponseDto> accountResponseDtoList = logService.searchBank(list);
+        return new ResponseEntity<>(accountResponseDtoList, HttpStatus.OK);
+    }
+}
