8039d7206c608ebbc86ff1a38a66612ab869af79
정현우 <gusehd502@naver.com>
2023-09-25 14:44:11 +0900

    feat : 문자 요청, 문자 인증 구현완료

--- Billage/Front/billage/src/type/auth.ts
+++ Billage/Front/billage/src/type/auth.ts
@@ -8,3 +8,13 @@ export interface LoginProps {
   userCellNo: string,
   userSimplePass: string,
 }
+
+export interface MessageProps{
+  to : string
+}
+
+export interface MessageCertProps{
+  phoneNumber : string,
+  verifyNumber : string
+}
+
