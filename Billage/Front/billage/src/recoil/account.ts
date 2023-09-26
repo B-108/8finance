a0606d1d487d2889b44f020836bf4ff13de16505
정현우 <gusehd502@naver.com>
2023-09-26 21:40:30 +0900

    feat : 간편비밀번호 입력 사이즈

--- Billage/Front/billage/src/recoil/account.ts
+++ Billage/Front/billage/src/recoil/account.ts
@@ -1,4 +1,12 @@
 import { atom } from "recoil";
 import { recoilPersist } from 'recoil-persist'
 
-// const { persistAtom } = recoilPersist();
+const { persistAtom } = recoilPersist();
+
+
+// 주계좌 담는 리코일 쓸지 말지 보류
+// export const MainAccountState = atom<string>({
+//   key: 'MainAccountState',
+//   default: "",
+//   effects_UNSTABLE: [persistAtom],
+// })
