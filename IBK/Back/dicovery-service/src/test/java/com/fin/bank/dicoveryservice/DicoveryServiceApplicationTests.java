4a7bfcf59a790c2324dfc9c7b75bea629a52f8ed
송혁준 <thdgurws98@gmail.com>
2023-09-26 17:45:54 +0900

    Build : ibk eureka server 230926 1545

---
+++ IBK/Back/dicovery-service/src/test/java/com/fin/bank/dicoveryservice/DicoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fin.bank.dicoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
