6278bd7e83a33b77f723f575170c1095aab4db29
daegyeong.lee <eorud2546@nate.com>
2023-09-27 07:40:15 +0900

    Fix : 계좌불러오기 수정

--- Billage/Back/billage/src/main/java/com/fin/billage/domain/transfer/repository/RequestUrlRepository.java
+++ Billage/Back/billage/src/main/java/com/fin/billage/domain/transfer/repository/RequestUrlRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface RequestUrlRepository extends JpaRepository <Url, Long> {
     Url findRequestUrlByRequestBankCodeAndRequestActCode(String requestBankCode, String requestActCode);
-    List<String> findRequestUrlsByRequestActCode(int actCode);
+//    List<String> findRequestUrlsByRequestActCode(int actCode);
 
+    Url findByRequestBankCodeAndRequestActCode(String bankCode, String s);
 }
