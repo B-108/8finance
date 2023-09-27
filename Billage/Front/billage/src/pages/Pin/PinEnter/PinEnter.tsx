8f8690758bcbb2049da1aa66f4aefd674a0567a1
정현우 <gusehd502@naver.com>
2023-09-28 00:24:18 +0900

    feat : Pin 자동 포커스 구현

--- Billage/Front/billage/src/pages/Pin/PinEnter/PinEnter.tsx
+++ Billage/Front/billage/src/pages/Pin/PinEnter/PinEnter.tsx
@@ -40,30 +40,45 @@ function PinEnter () {
   const moveMain = () => {navigate(`/Main`)}
 
   const handlepinEnterChange = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
-    if (event.target.value.length > 1){
-      event.target.value = event.target.value.slice(0,1)
+    if (event.target.value.length > 2){
+      event.target.value = event.target.value.slice(0,2)
       return 
     }
 
     if (event.target.value !== "") {
-      if (event.target.value === " "){ return }
-      setPinEnter(pinEnter + event.target.value);
+      if (event.target.value[1] === " "){ return }
+      if (event.target.value[1] === undefined) { 
+        setPinEnter(pinEnter.slice(0,pinEnter.length-2))
+        return
+      }
+      setPinEnter(pinEnter + event.target.value[1] + " ");
     }
-
+    
     else {
-      setPinEnter(pinEnter.slice(0,pinEnter.length-1))
+      if (pinEnter.length > 1) {
+        setPinEnter(pinEnter.slice(0,pinEnter.length-2))
+      }
     }
 
-    if (event.target.value.length === 1 && index < 4) {
+    if (event.target.value.length === 2 && index < 4) {
       const nextInput = inputRefs[index + 1].current;
+      
       if (nextInput) {
         nextInput.focus();
       }
     }
+
+    else if (!event.target.value.length && index > 0 && index < 6) {
+      const backInput = inputRefs[index - 1].current;
+
+      if (backInput) {
+        backInput.focus();
+      }
+    }
     
-    if (pinEnter.length >= 4 && event.target.value) {
+    if (pinEnter.length >= 8 && event.target.value) {
       if (routeAction === "login") {
-        axiosLogin(pinEnter + event.target.value)
+        axiosLogin(pinEnter.split(" ").join("") + event.target.value.split(" ").join(""))
       }
     }
   };
@@ -86,7 +101,7 @@ function PinEnter () {
     if (inputRefs[0].current) {
       inputRefs[0].current.focus();
     }
-    setPinEnter("")
+    setPinEnter(" ")
   }, []);
 
   return (
@@ -100,28 +115,28 @@ function PinEnter () {
           ref={inputRefs[0]}
           $size="20px," 
           $simplepassword
-          value={pinEnter.length >= 1 ? pinEnter[0] : ""}
+          value={pinEnter.length >= 1 ? pinEnter.slice(0,2) : ""}
           onChange={(event) => handlepinEnterChange(event, 0)}
         ></Input>
         <Input 
           ref={inputRefs[1]}
           $size="20px," 
           $simplepassword
-          value={pinEnter.length >= 2 ? pinEnter[1] : ""}
+          value={pinEnter.length >= 3 ? pinEnter.slice(2,4) : ""}
           onChange={(event) => handlepinEnterChange(event, 1)}
         ></Input>
         <Input 
           ref={inputRefs[2]}
           $size="20px," 
           $simplepassword
-          value={pinEnter.length >= 3 ? pinEnter[2] : ""}
+          value={pinEnter.length >= 5 ? pinEnter.slice(4,6) : ""}
           onChange={(event) => handlepinEnterChange(event, 2)}
         ></Input>
         <Input 
           ref={inputRefs[3]}
           $size="20px," 
           $simplepassword
-          value={pinEnter.length >= 4 ? pinEnter[3] : ""}
+          value={pinEnter.length >= 7 ? pinEnter.slice(6,8) : ""}
           onChange={(event) => handlepinEnterChange(event, 3)}
         ></Input>
         <Image
@@ -132,7 +147,7 @@ function PinEnter () {
           ref={inputRefs[4]}
           $size="20px," 
           $simplepassword
-          value={pinEnter.length >= 5 ? pinEnter[4] : ""}
+          value={pinEnter.length >= 9 ? pinEnter.slice(8,10) : ""}
           onChange={(event) => handlepinEnterChange(event, 4)}
         ></Input>
       </InputBox>
