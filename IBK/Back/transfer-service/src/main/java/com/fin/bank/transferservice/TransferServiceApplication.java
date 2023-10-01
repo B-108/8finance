7282cc8f7a104eb17b4420f86e78f5567020641b
송혁준 <thdgurws98@gmail.com>
2023-10-01 16:46:59 +0900

    Build : branch 변경 231001 1646

---
+++ IBK/Back/transfer-service/src/main/java/com/fin/bank/transferservice/TransferServiceApplication.java
@@ -0,0 +1,15 @@
+package com.fin.bank.transferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class TransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferServiceApplication.class, args);
+	}
+
+}
