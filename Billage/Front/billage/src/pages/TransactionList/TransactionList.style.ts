7472a28816eb266e8d14506bfa90eff54a96f637
정현우 <gusehd502@naver.com>
2023-10-02 22:43:45 +0900

    feat : 거래목록 CSS

---
+++ Billage/Front/billage/src/pages/TransactionList/TransactionList.style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const TitleBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0px 0px 15px 0px;
+  width: 94%;
+`
+
+export const ButtonBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 47%;
+
+`
