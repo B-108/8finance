807e7a38039d269c53d432c656f35c847d0c79bb
이주언 <wndjs8225@naver.com>
2023-10-02 18:19:44 +0900

    Billage feat fe sendmoney

--- Billage/Front/billage/src/pages/SendMoney/SendMoney.tsx
+++ Billage/Front/billage/src/pages/SendMoney/SendMoney.tsx
@@ -12,29 +12,91 @@ import { getAccountList } from '/src/api/account';
 import { AccountType } from '/src/type/account';
 import ConfirmBox from '/src/components/Common/YesOrNo';
 
+//이체
+import { SendMoneyType } from '/src/type/transaction';
+import { postSendMoney } from '/src/api/transaciton';
+
 function SendMoney() {
-    const [friendInfo, setFriendInfo] = useState<string>('');
-    const [accountInfo, setAccountInfo] = useState<string>('');
-    const [myAccountInfo, setMyAccountInfo] = useState<string>('');
-    const [amount, setAmountInfo] = useState<string>('0');
     const navigate = useNavigate()
     const location = useLocation()
+    //작성 취소 버튼 클릭시 활성
     const [isCancelDialogOpen, setIsCancelDialogOpen] = useState(false); // 다이얼로그 상태 추가
+    // 이체 필요 데이터
+    //거래ID
+    const [transId, setTransId] = useState<number>(0);
+    //보내는 사람(tranWd)
+    const [transWd, setTransWd] = useState<string>('');
+    //보내는 사람 계좌/은행 코드(tranWdAcNum, tranWdBankCode)
+    const [myAccountInfo, setMyAccountInfo] = useState<string>('');
+    const [myAccountInfoCode, setMyAccountInfoCode] = useState<string>('');
+    //받는 사람(tranDp)
+    const [friendInfo, setFriendInfo] = useState<string>('');
+    //받는 사람 계좌/은행 코드(tranDpAcNum, tranDpBankCode)
+    const [accountInfo, setAccountInfo] = useState<string>('');
+    const [accountInfoCode, setAccountInfoCode] = useState<string>('');
+    //금액(tranAmt)
+    const [amount, setAmountInfo] = useState<string>('0');
+    
 
+    console.log(location.state)
+    //내 계좌 목록
     const [accounts, setAccounts] = useState<AccountType[]>([])
+    
+    //Axios
     // 전체 계좌조회
     const axiosAccountList = async (): Promise<void> => {
         try {
         const response = await getAccountList()
         setAccounts(response?.data)
+        console.log(response?.data)
         }
         catch(error) {
         console.log(error)
         }
     }
-    // useEffect(()=>{
-    //     axiosAccountList()
-    //   },[])
+    //이체
+
+    const axiosSendMoney =async (): Promise<void> => {
+        const data :SendMoneyType ={
+            contractId : transId,
+            tranWd : transWd,
+            tranWdAcNum : myAccountInfo,
+            tranWdBankCode : myAccountInfoCode,
+            tranDp : friendInfo,
+            tranDpAcNum : accountInfo,
+            tranDpBankCode : accountInfoCode,
+            tranAmt : Number(amount),
+            tranContent : '돈 보내기'
+        }
+        try{
+            await postSendMoney(data)
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+
+    //useEffect
+    useEffect(()=>{
+        axiosAccountList()
+        setTransId(location.state.state.contractId)
+        setTransWd(location.state.state.debtoruser)
+        setFriendInfo(location.state.state.creditoruser)
+        setAccountInfo(location.state.detail.mainAccount)
+        setAccountInfoCode(location.state.detail.bankCode)
+      },[])
+
+    useEffect(() => {
+        const mainAccount = accounts.find(account => account.accountMainYn === true);
+        if (mainAccount) {
+            setMyAccountInfo(mainAccount.accountNum);
+            setMyAccountInfoCode(mainAccount.accountBankCode)
+        }
+    }, [accounts]);
+
+    console.log(accountInfoCode)
+    
+    //함수
 
     const handleCancelClick = () => {
         setIsCancelDialogOpen(true);
@@ -46,16 +108,9 @@ function SendMoney() {
 
     const moveToPinCheck = () => {
     };
-    
-    console.log(amount)
-    
-    const handleFriendInfoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFriendInfo(event.target.value);
-    };
-    const handleAccountInfoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setAccountInfo(event.target.value);
-    };
-    const handleMyAccountInfoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+    const handleMyAccountInfoChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        console.log(event.target.value); // 디버깅 목적
         setMyAccountInfo(event.target.value);
     };
     const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -74,7 +129,7 @@ function SendMoney() {
                     value={friendInfo}
                     $active
                     $size="88%,40px"
-                    onChange={handleFriendInfoChange}
+                    disabled
                     />
             </InputDiv>
             <hr />
@@ -84,31 +139,21 @@ function SendMoney() {
                     value={accountInfo}
                     $active
                     $size="88%,40px"
-                    onChange={handleAccountInfoChange}
+                    disabled
                     />
             </InputDiv>
             <hr />
             <InputDiv style={{alignItems:"center"}}>
                 <InputTitle>내 계좌</InputTitle>
-                {/* <ButtonInput
-                    value={myAccountInfo}
-                    $active
-                    $size="88%,40px"
-                    onChange={handleMyAccountInfoChange}
-                    $buttonImage={plus} // 이미지 버튼 추가
-                /> */}
                     <select
                     value={myAccountInfo}
-                    onChange={() => handleMyAccountInfoChange}
-                    style={{width : '95%', height: '40px', borderRadius: '10px', border: '3px solid'}}
-                    // $active
-                    // $size="86%,40px"
+                    onChange={handleMyAccountInfoChange}
+                    style={{width : '95%', height: '40px', borderRadius: '10px', border: '3px solid #BDBDBD'}}
                 >
                     {accounts.map((account) => (
                         <option
                             key={account.accountId}
                             value={account.accountNum}
-                            disabled={!account.accountMainYn}
                         >
                             {account.accountNum}
                         </option>
@@ -154,7 +199,13 @@ function SendMoney() {
             <hr />
             <InputDiv style={{alignItems:"center"}}>
                 <InputTitle>남은 금액</InputTitle>
-                {/* <Input $active $size="88%,40px">{location.state.repaymentCash - Number(amount)}</Input> */}
+                <Input
+                  value={location.state.detail.repaymentCash - Number(amount)} 
+                  $active 
+                  $size="88%,40px" 
+                  $position
+                  disabled
+                  ></Input>
             </InputDiv>
             <hr />
             <ButtonContainer>
