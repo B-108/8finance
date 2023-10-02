c8a2f52027e4b54f682a2d36ffd21d5c7e17cbc5
송혁준 <thdgurws98@gmail.com>
2023-10-03 03:06:08 +0900

    Feat : auth filter 기능 추가 231003 0306

--- IBK/Back/gateway-service/src/main/java/com/fin/bank/gatewayservice/GatewayServiceApplication.java
+++ IBK/Back/gateway-service/src/main/java/com/fin/bank/gatewayservice/GatewayServiceApplication.java
@@ -3,6 +3,9 @@ package com.fin.bank.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +15,8 @@ public class GatewayServiceApplication {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security) {
+		return security.csrf().disable().build();
+	}
 }
