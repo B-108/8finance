13e63327a40076433380b20eae4aaac73870759a
정현우 <gusehd502@naver.com>
2023-10-04 02:49:09 +0900

    feat : 알림상세 페이지만

--- Billage/Front/billage/src/pages/Notifications/Notifications.tsx
+++ Billage/Front/billage/src/pages/Notifications/Notifications.tsx
@@ -1,26 +1,51 @@
 import { useState } from 'react';
-import Notification from '/src/components/Notification/Notification';
+
+// 재사용 컴포넌트
 import Header from '/src/components/Header/Header';
 import CenteredContainer from '/src/components/Common/CenterAlign';
+import Image from '/src/components/Common/Image';
+
+// 스타일 컴포넌트
+import { 
+  Content, 
+  Date, 
+  LeftSection, 
+  NotiBox, 
+  NotiContainer } from './Notifications.style';
+
+// 이미지
+import alramClock from '/src/assets/alramClock.svg'
+
+const dummy = [
+  "정싸피님이 빌린돈을 갚았습니다.",
+  "정싸피님이 빌린돈을 갚았습니다.",
+  "정싸피님이 빌린돈을 갚았습니다.",
+]
 
 function Notifications() {
-    const data = [
-        {
-            data: 1,
-        },
-    ];
-
-    const [notificationInfo, setNotificationInfo] = useState<string>('');
-
     return (
         <CenteredContainer>
             <Header
-                headerTitle="알림
-            "
-            ></Header>
-            <div style={{ display: 'flex' }}>
-                <Notification></Notification>
-            </div>
+                headerTitle="알림"></Header>
+            <NotiContainer>
+              {dummy.map((noti,index) => (
+                <NotiBox 
+                  key={index}>
+                  <LeftSection>
+                    <Image
+                      src={alramClock}
+                      alt="alarmBell"
+                      width='11%'></Image>
+
+                    <Content>
+                      {noti}</Content>
+                  </LeftSection>
+
+                  <Date>
+                    오늘</Date>
+                </NotiBox>
+              ))}
+            </NotiContainer>
         </CenteredContainer>
     );
 }
