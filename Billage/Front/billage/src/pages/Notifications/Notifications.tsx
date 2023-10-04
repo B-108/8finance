89b01d045fa188be3ab4dc482fa31188555fc5e3
정현우 <gusehd502@naver.com>
2023-10-04 17:51:24 +0900

    feat : 알람 시간 업데이트

--- Billage/Front/billage/src/pages/Notifications/Notifications.tsx
+++ Billage/Front/billage/src/pages/Notifications/Notifications.tsx
@@ -9,14 +9,13 @@ import Image from '/src/components/Common/Image';
 import { 
   Check,
   ContentBox, 
-  Date, 
+  DateBox, 
   LeftSection, 
   Noti, 
   NotiBox, 
   NotiContainer } from './Notifications.style';
 
 // 이미지
-import alramClock from '/src/assets/alramClock.svg'
 import rightarrow_bank from '/src/assets/rightarrow_bank.svg'
 import won from "/src/assets/won.svg"
 import total from "/src/assets/receiveMoney.svg"
@@ -29,7 +28,6 @@ import {
   patchNotifiCation } from '/src/api/noti';
 
 // 타입스크립트
-import { NotificationType } from '/src/type/noti';
 
 // 리코일
 import { useRecoilState } from 'recoil';
@@ -37,6 +35,37 @@ import { NotificationState } from '/src/recoil/noti';
 
 function Notifications() {
     const [notifications, setNotifications] = useRecoilState(NotificationState)
+
+    const TimeDisplay = (noticeSendDate) => {
+      const currentTime = new Date();
+      const targetTime = new Date(noticeSendDate);
+      
+      const timeDifference = currentTime - targetTime;
+      const minute = 60 * 1000;
+      const hour = minute * 60;
+      const day = hour * 24;
+      
+        if (timeDifference < minute) {
+          return ('1분 전');
+        } else if (timeDifference < hour) {
+          const diffMinutes = Math.floor(timeDifference / minute);
+          return(`${diffMinutes}분 전`);
+        } else if (timeDifference < day) {
+          const diffHours = Math.floor(timeDifference / hour);
+          return(`${diffHours}시간 전`);
+        } else if (timeDifference < day * 7) {
+          const diffDays = Math.floor(timeDifference / day);
+          return(`${diffDays}일 전`);
+        } else {
+          const year = targetTime.getFullYear();
+          const month = targetTime.getMonth() + 1;
+          const day = targetTime.getDate();
+          const hours = targetTime.getHours();
+          const minutes = targetTime.getMinutes();
+
+          return(`${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분`);
+          }
+      }
 
     // 알람목록조회
     const axiosNotifiCation = async (): Promise<void> => {
@@ -84,7 +113,7 @@ function Notifications() {
                       <Check $IsClick={noti.noticeState}/>
                       <ContentBox>
                         <Noti>"{noti.noticeUserName}"님에게 {noti.noticeAmount}원을 빌려달라는 요청이 왔어요.</Noti>
-                        <Date>{noti.noticeSendDate}</Date>
+                        <DateBox>{TimeDisplay(noti.noticeSendDate)}</DateBox>
                       </ContentBox>
                     </LeftSection>
                   ) : (
@@ -97,7 +126,7 @@ function Notifications() {
                       <Check $IsClick={noti.noticeState}/>
                       <ContentBox>
                         <Noti>"{noti.noticeUserName}"님이 {noti.noticeAmount}원을 빌려줬어요!</Noti>
-                        <Date>{noti.noticeSendDate}</Date>
+                        <DateBox>{TimeDisplay(noti.noticeSendDate)}</DateBox>
                       </ContentBox>
                     </LeftSection>
                   ) : (
@@ -110,7 +139,7 @@ function Notifications() {
                       <Check $IsClick={noti.noticeState}/>
                       <ContentBox>
                         <Noti>"{noti.noticeUserName}"님이 돈빌려주는 것을 거절했어요.</Noti>
-                        <Date>{noti.noticeSendDate}</Date>
+                        <DateBox>{TimeDisplay(noti.noticeSendDate)}</DateBox>
                       </ContentBox>
                     </LeftSection>
                   ) : (
@@ -122,7 +151,7 @@ function Notifications() {
                       <Check $IsClick={noti.noticeState}/>
                       <ContentBox>
                         <Noti>"{noti.noticeUserName}"님이 {noti.noticeAmount}원을 갚았어요!</Noti>
-                        <Date>{noti.noticeSendDate}</Date>
+                        <DateBox>{TimeDisplay(noti.noticeSendDate)}</DateBox>
                       </ContentBox>
                     </LeftSection>
                       )))}
