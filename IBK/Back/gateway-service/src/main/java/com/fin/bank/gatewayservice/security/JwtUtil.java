a927619981f5819e4ecf8a38429dcceb1f4a6dd0
송혁준 <thdgurws98@gmail.com>
2023-10-05 03:46:27 +0900

    Fix : filter 제거 231005 0346

--- IBK/Back/gateway-service/src/main/java/com/fin/bank/gatewayservice/security/JwtUtil.java
+++ IBK/Back/gateway-service/src/main/java/com/fin/bank/gatewayservice/security/JwtUtil.java
@@ -1,55 +1,55 @@
-package com.fin.bank.gatewayservice.security;
-
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import java.security.Key;
-import java.util.HashMap;
-import java.util.Map;
-import javax.xml.bind.DatatypeConverter;
-
-@Slf4j
-@Component
-public class JwtUtil {
-    private final Key key;
-
-    public JwtUtil(@Value("${jwt.secret}") String secretKey) {
-        byte[] secretByteKey = DatatypeConverter.parseBase64Binary(secretKey);
-        this.key = Keys.hmacShaKeyFor(secretByteKey);
-    }
-
-    public boolean isJwtValid(String jwt) {
-        try {
-            Jwts.parser()
-                    .setSigningKey(key)
-                    .parseClaimsJws(jwt);
-            return true;
-        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
-            log.info("Invalid JWT Token", e);
-        } catch (ExpiredJwtException e) {
-            log.info("Expired JWT Token", e);
-        } catch (UnsupportedJwtException e) {
-            log.info("Unsurported JWT Token", e);
-        } catch (IllegalArgumentException e) {
-            log.info("JWT claims string is empty", e);
-        }
-        return false;
-    }
-
-    public Map<String, String> parseClaims(String token) {
-        Claims parse = Jwts.parser().setSigningKey(key)
-                .parseClaimsJws(token)
-                .getBody();
-
-        Map<String, String> result = new HashMap<>();
-
-        result.put("userPk", parse.get("userPk").toString());
-//        result.put("accountNo", parse.get("accountNo").toString());
-//        result.put("scope", parse.get("scope").toString());
-
-        return result;
-    }
-}
+//package com.fin.bank.gatewayservice.security;
+//
+//import io.jsonwebtoken.*;
+//import io.jsonwebtoken.security.Keys;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//import java.security.Key;
+//import java.util.HashMap;
+//import java.util.Map;
+//import javax.xml.bind.DatatypeConverter;
+//
+//@Slf4j
+//@Component
+//public class JwtUtil {
+//    private final Key key;
+//
+//    public JwtUtil(@Value("${jwt.secret}") String secretKey) {
+//        byte[] secretByteKey = DatatypeConverter.parseBase64Binary(secretKey);
+//        this.key = Keys.hmacShaKeyFor(secretByteKey);
+//    }
+//
+//    public boolean isJwtValid(String jwt) {
+//        try {
+//            Jwts.parser()
+//                    .setSigningKey(key)
+//                    .parseClaimsJws(jwt);
+//            return true;
+//        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+//            log.info("Invalid JWT Token", e);
+//        } catch (ExpiredJwtException e) {
+//            log.info("Expired JWT Token", e);
+//        } catch (UnsupportedJwtException e) {
+//            log.info("Unsurported JWT Token", e);
+//        } catch (IllegalArgumentException e) {
+//            log.info("JWT claims string is empty", e);
+//        }
+//        return false;
+//    }
+//
+//    public Map<String, String> parseClaims(String token) {
+//        Claims parse = Jwts.parser().setSigningKey(key)
+//                .parseClaimsJws(token)
+//                .getBody();
+//
+//        Map<String, String> result = new HashMap<>();
+//
+//        result.put("userPk", parse.get("userPk").toString());
+////        result.put("accountNo", parse.get("accountNo").toString());
+////        result.put("scope", parse.get("scope").toString());
+//
+//        return result;
+//    }
+//}
