a5ffadcc20d850e96dd78487de86cb710530c8fa
송혁준 <thdgurws98@gmail.com>
2023-10-04 13:32:28 +0900

    Fix : response DTO 변경 231004 1332

--- IBK/Back/transfer-service/src/main/java/com/fin/bank/transferservice/transfer/dto/AccountResponseDto.java
+++ IBK/Back/transfer-service/src/main/java/com/fin/bank/transferservice/transfer/dto/AccountResponseDto.java
@@ -1,11 +1,15 @@
 package com.fin.bank.transferservice.transfer.dto;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class AccountResponseDto {
 
     private Long accountId;
+
+    @Builder
+    public AccountResponseDto(Long accountId) {
+        this.accountId = accountId;
+    }
 }
