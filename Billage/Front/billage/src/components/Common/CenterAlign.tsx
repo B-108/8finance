4c9bb36c64e7cdc8732cfbb511c1caafc7c287a5
정현우 <gusehd502@naver.com>
2023-10-06 10:13:27 +0900

    feat : 화면전체길이 수정

--- Billage/Front/billage/src/components/Common/CenterAlign.tsx
+++ Billage/Front/billage/src/components/Common/CenterAlign.tsx
@@ -19,7 +19,7 @@ export const Container = styled.div<InputContainer>`
     position: relative;
     background-color: white;
     justify-content: ${(props) => props.$center ? "center" : ""};
-    height: 710px; /* 화면 높이의 100% */
+    height: 1000px; /* 화면 높이의 100% */
     overflow-y: scroll;
     
     &::-webkit-scrollbar {
